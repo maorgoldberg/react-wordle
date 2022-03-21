83b57a4ab873f2e228845000e7e2b8657d53e0c9
maorgoldberg <maorgoldberg3@gmail.com>
2022-03-21 11:56:17 +0200

    add red fill on error

--- src/components/grid/CurrentRow.tsx
+++ src/components/grid/CurrentRow.tsx
@@ -1,6 +1,6 @@
 import { MAX_WORD_LENGTH } from '../../constants/settings'
 import { Cell } from './Cell'
-import { unicodeSplit } from '../../lib/words'
+import { unicodeSplit, isWordInWordList } from '../../lib/words'
 
 type Props = {
   guess: string
@@ -11,11 +11,12 @@ export const CurrentRow = ({ guess, className }: Props) => {
   const splitGuess = unicodeSplit(guess)
   const emptyCells = Array.from(Array(MAX_WORD_LENGTH - splitGuess.length))
   const classes = `flex justify-center mb-1 ${className}`
+  const valid = emptyCells.length !== 0 ? true : isWordInWordList(guess) ? true : false;
 
   return (
     <div className={classes}>
       {splitGuess.map((letter, i) => (
-        <Cell key={i} value={letter} />
+        <Cell key={i} value={letter} valid={valid}/>
       ))}
       {emptyCells.map((_, i) => (
         <Cell key={i} />
--- src/components/grid2/CurrentRow.tsx
+++ src/components/grid2/CurrentRow.tsx
@@ -1,6 +1,6 @@
 import { MAX_WORD_LENGTH } from '../../constants/settings'
 import { Cell2 } from './Cell'
-import { unicodeSplit } from '../../lib/words'
+import { unicodeSplit, isWordInWordList } from '../../lib/words'
 
 type Props = {
   guess: string
@@ -11,11 +11,11 @@ export const CurrentRow2 = ({ guess, className }: Props) => {
   const splitGuess = unicodeSplit(guess)
   const emptyCells = Array.from(Array(MAX_WORD_LENGTH - splitGuess.length))
   const classes = `flex justify-center mb-1 ${className}`
-
+  const valid = emptyCells.length !== 0 ? true : isWordInWordList(guess) ? true : false;
   return (
     <div className={classes}>
       {splitGuess.map((letter, i) => (
-        <Cell2 key={i} value={letter} />
+        <Cell2 key={i} value={letter} valid={valid}/>
       ))}
       {emptyCells.map((_, i) => (
         <Cell2 key={i} />
